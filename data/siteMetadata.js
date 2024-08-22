592a69615f53136a82408af0022572e46467f931
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2024-08-22 23:00:44 +1000

    update post images

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'BytesBlogger',
   author: 'Rakesh Lakshminarayana',
   headerTitle: 'BytesBlogger',
   description: 'A blog created with Next.js and Tailwind.css',
